c86c134f57cad686dcc4387108143cd53c90345c
Aman Rohilla <rohillaaman69@gmail.com>
2025-06-28 23:09:31 +0530

    updated the index.js file with latest changes

--- index.js
+++ index.js
@@ -36,7 +36,20 @@ app.listen(process.env.PORT, () => {
 import "dotenv/config";
 const apps = express();
 const PORTS = process.env.PORTS || 9000;
-
+const apidata = [
+  {
+    username: "aman123",
+    password: "abc@123",
+    email: "aman@example.com",
+    lastLogin: "2025-06-28T10:00:00Z",
+  },
+  {
+    username: "rohit456",
+    password: "xyz@456",
+    email: "rohit@example.com",
+    lastLogin: "2025-06-27T08:30:00Z",
+  },
+];
 apps.get("/", (req, res) => {
   res.send("Hello aman!");
 });
@@ -45,13 +58,15 @@ apps.listen(process.env.PORTS, () => {
 });
 apps.get("/twitter", (req, res) => {
   res.send("<h1>login page of admin!</h1> \n hello twitter!");
-
 });
 apps.get("/login", (req, res) => {
   res.send(
     "<h1>Login Page</h1><form action='/login' method='post'><input type='text' name='username' placeholder='Username'><input type='password' name='password' placeholder='Password'><button type='submit'>Login</button></form>"
   );
 });
+apps.get("/apidata",(req,res)=>(
+  res.json(apidata)
+))
 apps.listen(process.env.PORTS, () => {
   console.log(`Server is running on http://localhost:${PORTS}`);
 });
